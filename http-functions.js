4fbed1a4133e1c896ea76246fc4803cc5d3786db
Garo-A <garo.assilian@gmail.com>
2017-07-03 16:08:24 +0000

    This now has the modular getHTML function

--- http-functions.js
+++ http-functions.js
@@ -1,20 +1,6 @@
-var https = require("https");
+var https = require('https');
 
-var args = process.argv.slice(2) //takes arguments from terminal.
-
-console.log(args[0]);
-console.log(args[1]);
-
-function printHTML(html){ //My callback function; this will be called upon at some point.
-  console.log(html);
-}
-
-var options = { //This is where the information about the given website and path who's information i want.
-  host: args[0],
-   path: args[1]
-}
-
-function getHTML(requests, callback){
+module.exports = function getHTML(requests, callback){
 
   var data = "";
 
@@ -38,7 +24,3 @@ function getHTML(requests, callback){
   })
 
 }
-
-getHTML(options, printHTML);
-
-
